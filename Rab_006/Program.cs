d1d6c82b06325f46118aba7476e0ba05b4aa1cd2
Level-999 <dalsep@mail.ru>
2021-12-11 22:36:16 +1000

    Добавьте файлы проекта.

---
+++ Rab_006/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Rab_006_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввести с клавиатуры предложение. Предложение представляет собой слова,
+            //разделенные пробелом. Знаки препинания не используются.
+            //Найти самое длинное слово в строке.
+
+            Console.WriteLine("Введите с клавиатуры предложение");
+            Console.WriteLine();
+            string str = Console.ReadLine();
+            string[] word = str.Split(' ');
+
+            string a = "";
+            int b = 0;
+            foreach (string s in word)
+            {
+                if (s.Length > b)
+
+                {
+                    a = s;
+                    b = s.Length;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Самое длинное слово в предложении");
+            Console.WriteLine();
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rab_006_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Rab_006_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввести с клавиатуры предложение. Предложение представляет собой слова,
+            //разделенные пробелом. Знаки препинания не используются. Составить программу,
+            //определяющую является ли строка палиндромом без учёта пробелов и регистра
+            //(пример палиндрома – «арозаупаланалапуазора»).
+
+            string str = Console.ReadLine();
+            string revStr = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revStr += str[i];
+            }
+            Console.WriteLine();
+            if (str == revStr)
+                Console.WriteLine("Строка является палиандромом");
+            else
+                Console.WriteLine("Строка не является палиандромом");
+
+
+            Console.ReadKey();
+        }
+    }
+}
